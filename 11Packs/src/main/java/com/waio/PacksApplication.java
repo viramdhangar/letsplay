fabaf33d583c8bc0c57660111f26b184dcf238bb
Viram Dhangar <viram.dhangar@gmail.com>
2018-12-06 10:24:03 +0530

    First commit added comment

--- 11Packs/src/main/java/com/waio/PacksApplication.java
+++ 11Packs/src/main/java/com/waio/PacksApplication.java
@@ -2,7 +2,10 @@ package com.waio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ * Author: Viram Dhangar
+ * This class is a spring boot configuration class
+ * */
 @SpringBootApplication
 public class PacksApplication {
 
